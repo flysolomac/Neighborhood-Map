1d06afdf6f25a5d4d4d11c36d3c801fa895b7567
flysolomac <macbaksh@gmail.com>
2015-08-13 06:20:51 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Frontend Nanodegree Neighborhood Map Project
+
+Project 5 can be viewed:
+
+Used Google Maps API and Yelp API in order to render neighboorhood map. Knockout framework was used to bind data while bootstrap was used to style page.
+
+Search bar located at top with favorite restaurant defaulted to Wendy's.
+
+List of search results will be displayed on the left and list is scrollable.
---
+++ js/function.js
@@ -0,0 +1,31 @@
+/*
+ *	create knockout model to bind to the search element
+ */												
+function yelpViewModel() {
+	// set this to the self variable
+  var self = this;
+
+	/*
+	 *	set the bind-data to the search field to "Wendy's"
+	 *	observe for change
+	 */	
+	self.searchTerm = ko.observable("Wendy's");
+
+	/*
+	 *	function to update the view model
+	 */	
+	self.updateYelpResults = function(){
+		/*
+		 *	return the updated data from the search field
+		 *	then run the ajax function to create the yelp list
+		 */	
+		ko.computed(function(){
+			yelpCall('30308', self.searchTerm());
+		}, self);
+	}	
+}
+
+/*
+ *	Start knockout dependency tracking
+ */	
+ko.applyBindings(new yelpViewModel());
