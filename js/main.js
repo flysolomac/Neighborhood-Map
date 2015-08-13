ff6ba5e4dc25dcc951eacd30382ccd645f75bad9
flysolomac <macbaksh@gmail.com>
2015-08-13 18:37:05 -0400

    js

--- js/main.js
+++ js/main.js
@@ -356,7 +356,7 @@ function makeYelpList(d) {
       
       var src = $('.image-thumbnail').attr("src");
       console.log(src);
-      if (src === undefined || src === null){
+      if (src !=== 'string'){
     /*
      *	If img source is returned undefined 
      */
