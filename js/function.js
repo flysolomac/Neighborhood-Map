ba8c41c82cb8f5575db446fff5358a9b461104ef
flysolomac <macbaksh@gmail.com>
2015-09-09 01:35:48 -0400

    Update function.js

--- js/function.js
+++ js/function.js
@@ -9,6 +9,7 @@ function yelpViewModel() {
      *	set the bind-data to the search field to "Wendy's"
      *	observe for change
      */
+    self.myFav = ko.observable("My Places");
     self.searchTerm = ko.observable("Wendy's");
     self.zipCode = ko.observable("30308");
     /*
@@ -24,50 +25,16 @@ function yelpViewModel() {
         }, self);
     }
 
-    self.sortRate = function() {
+    self.sortFavorite = function() {
         /*
-         *	return the updated data from the search field
-         *	then run the ajax function to create the yelp list
+         *	returns my favorite loations
+         *
          */
         ko.computed(function() {
-            yelpRate(self.zipCode(), self.searchTerm());
+            sortFavorite();
         }, self);
     }
 
-}
-
-
-
-
-/*
- *	Start knockout dependency tracking
- */
-ko.applyBindings(new yelpViewModel());
-/*
- *	create knockout model to bind to the search element
- */
-function yelpViewModel() {
-    // set this to the self variable
-    var self = this;
-
-    /*
-     *	set the bind-data to the search field to "Wendy's"
-     *	observe for change
-     */
-    self.searchTerm = ko.observable("Wendy's");
-
-    /*
-     *	function to update the view model
-     */
-    self.updateYelpResults = function() {
-        /*
-         *	return the updated data from the search field
-         *	then run the ajax function to create the yelp list
-         */
-        ko.computed(function() {
-            yelpCall('30308', self.searchTerm());
-        }, self)
-    }
 }
 
 /*
